9a0c3374a26dab8f1f91bf6718aa92d89b1680f9
viraj8206 <109304745+viraj8206@users.noreply.github.com>
2024-07-10 23:11:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.getElementById('cv_download').addEventListener('click', function() {
     // Create an anchor element (a) to trigger the download
     var downloadAnchor = document.createElement('a');
     downloadAnchor.setAttribute('href', '../asset/cv/'); // Replace with your file path
-    downloadAnchor.setAttribute('download', 'resume.pdf'); // Replace with desired file name
+    downloadAnchor.setAttribute('download', 'Faldu Viraj.pdf'); // Replace with desired file name
 
     // Simulate click of anchor to trigger download
     downloadAnchor.click();
