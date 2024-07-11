a505c9ccfe9dae709b8b9ee425d4362f0c41a687
viraj8206 <109304745+viraj8206@users.noreply.github.com>
2024-07-11 22:13:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
-document.getElementById('cv_download').addEventListener('click', function() {
+document.getElementById('cv_download').addEventListener('click', function(event) {
     // Replace with your file URL
-    var fileUrl = 'main/assets/cv/';
-  
-    // Use an anchor element to initiate download
-    var a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = fileUrl;
-  
-    // Set the file name (you can set dynamically if needed)
-    a.download = 'Faldu Viraj.pdf'; // Example filename
-  
-    // Append anchor to body and trigger click event
-    document.body.appendChild(a);
-    a.click();
-  
-    // Clean up
-    document.body.removeChild(a);
+    const filename = "assets/cv/Faldu Viraj.pdf";
+    this.href = filename;
   });
   
