5c1437d1e96a53ad033dc6909f43bcba75c6b4e2
viraj8206 <109304745+viraj8206@users.noreply.github.com>
2025-03-06 22:07:38 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 document.getElementById('cv_download').addEventListener('click', function(event) {
     // Replace with your file URL
-    const filename = "assets/cv/Faldu Viraj.pdf";
+    const filename = "assets/cv/Faldu viraj new resume.pdf";
     this.href = filename;
   });
 
 document.getElementById('cv_dwd').addEventListener('click', function(event) {
     // Replace with your file URL
-    const filename = "assets/cv/Faldu Viraj.pdf";
+    const filename = "assets/cv/Faldu viraj new resume.pdf";
     this.href = filename;
   });
   
